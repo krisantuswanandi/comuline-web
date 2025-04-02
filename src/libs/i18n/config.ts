d9612242c58e9e5d87006d2eb18bcec7fe9c529b
rioarray <rio.array@windowslive.com>
2025-04-02 19:27:29 +0700

    change default lng translation into id

--- src/libs/i18n/config.ts
+++ src/libs/i18n/config.ts
@@ -3,7 +3,7 @@ import { initReactI18next } from "react-i18next";
 import en from "./en/translation.json";
 
 i18n.use(initReactI18next).init({
-  lng: "cimode",
+  lng: "id",
   interpolation: {
     escapeValue: false,
   },
